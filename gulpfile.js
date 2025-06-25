b66fd5cc2c4da2f3c427a5838e87115e18086c35
vitordrs <soaresvitor196@gmail.com>
2025-06-25 15:01:34 -0300

    Criando hero e importante fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles(){
     return gulp.src('./src/style/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
+function images(){
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
 
 
-exports.default = styles;
+exports.default = gulp.parallel (styles, images);
 exports.watch =  function(){
     gulp.watch('./src/style/*.scss', gulp.parallel(styles))
 }
