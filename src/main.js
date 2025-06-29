4fc7296656921d2099eaa9c4b11d305852ae0134
vitordrs <soaresvitor196@gmail.com>
2025-06-29 13:30:55 -0300

    adicionando favicon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     const heroSection = document.querySelector('.hero');
     const alturaHero =  heroSection.clientHeight;
